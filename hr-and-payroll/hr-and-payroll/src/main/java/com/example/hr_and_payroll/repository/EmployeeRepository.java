3f19350110305589ae554a851362fba10bb9ffad
niko.troqe <troqeniko@gmail.com>
2024-07-11 12:29:30 +0200

    added relation between employee and PerformanceReview entity

--- hr-and-payroll/hr-and-payroll/src/main/java/com/example/hr_and_payroll/repository/EmployeeRepository.java
+++ hr-and-payroll/hr-and-payroll/src/main/java/com/example/hr_and_payroll/repository/EmployeeRepository.java
@@ -4,7 +4,10 @@ import com.example.hr_and_payroll.domain.entity.Employee;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface EmployeeRepository extends JpaRepository<Employee, Integer> {
+    List<Employee> findByFirstNameContainingIgnoreCaseOrLastNameContainingIgnoreCase(String firstName, String lastName);
 
 }
