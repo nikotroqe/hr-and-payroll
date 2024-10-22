53e56adde302d727794bc24b862cfdf1953d796e
niko.troqe <troqeniko@gmail.com>
2024-10-22 13:38:50 +0200

    added relation between employee and PerformanceReview entity

---
+++ hr-and-payroll/hr-and-payroll/src/main/java/com/example/hr_and_payroll/mapper/OvertimeMapper.java
@@ -0,0 +1,24 @@
+package com.example.hr_and_payroll.mapper;
+
+import com.example.hr_and_payroll.domain.dto.OvertimeDTO;
+import com.example.hr_and_payroll.domain.entity.Employee;
+import com.example.hr_and_payroll.domain.entity.Overtime;
+
+public class OvertimeMapper {
+    public static OvertimeDTO mapToOvertimeDto(Overtime overtime){
+        return new OvertimeDTO(
+                overtime.getId(),
+                overtime.getEmployee().getId(),
+                overtime.getOvertimeHours(),
+                overtime.isApproved()
+        );
+    }
+    public static Overtime mapToOvertime(OvertimeDTO overtimeDTO){
+        return new Overtime(
+                new Employee(
+                        overtimeDTO.getEmployeeId()),
+                overtimeDTO.getOvertimeHours(),
+                overtimeDTO.isApproved()
+        );
+    }
+}
